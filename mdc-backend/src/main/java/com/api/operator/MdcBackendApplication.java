521fb3d7664c4646744d5953ad40d5dfcb619fe3
Shivam Yadav <yadavshivam24012002@gmail.com>
2023-04-03 15:46:47 +0530

    project initialised

---
+++ mdc-backend/src/main/java/com/api/operator/MdcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.operator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdcBackendApplication.class, args);
+	}
+
+}
---
+++ mdc-backend/src/test/java/com/api/operator/MdcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.operator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
